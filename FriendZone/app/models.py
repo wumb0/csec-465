f3154cf867eae0323aefc1e63c205e5f466f5080
Edward Mead <exm6939@rit.edu>
2015-11-09 18:55:59 -0500

    Merged with up to date code

--- FriendZone/app/models.py
+++ FriendZone/app/models.py
@@ -1,11 +1,83 @@
-from app import db
+#from app import db
 from datetime import datetime
+from elasticsearch_dsl import DocType, String, Date, Integer
+from elasticsearch_dsl.connections import connections
+
+connections.create_connection(hosts=['localhost'])
 
 friends_table = db.Table('friends_table',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('friend_id', db.Integer, db.ForeignKey('user.id')),
 )
 
+class Article(DocType):
+    title = String(analyzer='snowball', fields={'raw': String(index='not_analyzed')})
+    body = String(analyzer='snowball')
+    tags = String(index='not_analyzed')
+    published_from = Date()
+    lines = Integer()
+
+    class Meta:
+        index = 'blog'
+
+    def save(self, ** kwargs):
+        self.lines = len(self.body.split())
+        return super(Article, self).save(** kwargs)
+
+    def is_published(self):
+        return datetime.now() < self.published_from
+
+class User_ES(DocType):
+    name = String(analyzer='snowball', fields={'raw': String(index='not_analyzed')})
+    nickname = String(analyzer='snowball')
+    email = String(analyzer='snowball')
+    password = String(analyzer='snowball')
+    role = Integer()
+    bio = String(analyzer='snowball')
+    birthday = Date()
+    last_seen = Date()
+    verified =
+
+    email = String(analyzer='snowball')
+    email = String(analyzer='snowball')
+    published_from = Date()
+    lines = Integer()
+
+    class Meta:
+        index = 'blog'
+
+    def save(self, ** kwargs):
+        self.lines = len(self.body.split())
+        return super(User, self).save(** kwargs)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def is_admin(self):
+        if self.role == 1:
+            return True
+        return False
+
+    def bday_str(self):
+        return self.birthday.strftime('%B %-d, %Y')
+
+    def __repr__(self):
+        return '<User {}: {}>'.format(self.id, self.name)
+
+    def __str__(self):
+        return self.name
+
+    def last_seen_str(self):
+        return self.last_seen.strftime('%A, %B %d %Y %I:%M%p')
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
